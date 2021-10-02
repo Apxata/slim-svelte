00515232c65d37be19fee956fc3ef80b61e08248
Apxat <apxata@gmail.com>
2021-10-02 10:15:58 +0300

    config

--- public/index.php
+++ public/index.php
@@ -1,18 +1,36 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+declare(strict_types=1);
+
+use DI\Container;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new Container();
+
+$settings = require __DIR__ . '/../app/settings.php';
+$settings($container);
+
+$connection = require __DIR__ . '/../app/connection.php';
+$connection($container);
+
+$logger = require __DIR__ . '/../app/logger.php';
+$logger($container);
+
+// Set Container on app
+AppFactory::setContainer($container);
+
+// Create App
 $app = AppFactory::create();
 
-$app->get('/api', function (Request $request, Response $response, $args) {
-    $data = ('hello world');
-    $payload = json_encode($data);
-    $response->getBody()->write($payload);
-    return $response
-        ->withHeader('Content-Type', 'application/json');
-});
+$views = require __DIR__ . '/../app/views.php';
+$views($app);
 
+$middleware = require __DIR__ . '/../app/middleware.php';
+$middleware($app);
+
+$routes = require __DIR__ . '/../app/routes.php';
+$routes($app);
+
+// Run App
 $app->run();
