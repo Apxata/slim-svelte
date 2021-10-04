fa8c0ac66fef690d79e40ff17b525db961d17268
Apxat <apxata@gmail.com>
2021-10-04 14:36:25 +0300

    env file load

--- api/config/common/system.php
+++ api/config/common/system.php
@@ -2,8 +2,21 @@
 
 declare (strict_types=1);
 
+use Dotenv\Dotenv;
+
+include_once __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(dirname(dirname(__DIR__)));
+$dotenv->load();
+
+if ($_ENV['APP_ENV'] === 'DEV') {
+    $debug = true;
+} else {
+    $debug = false;
+}
+
 return [
     'config' => [
-        'debug' => true
+        'debug' => $debug
     ],
 ];
